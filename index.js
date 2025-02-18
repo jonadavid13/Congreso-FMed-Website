43427075a55da91448ec1e5268acbdb8939657a1
Jonathan Núñez <jonadavid13@gmail.com>
2025-02-18 04:06:08 -0400

    update: sección Trabajos Libres agregada (info + archivo de descarga)

--- index.js
+++ index.js
@@ -1,23 +1,43 @@
 let isAnimating = false
 
-document.getElementById('navLink-Inscripciones').addEventListener('click', function() {
-    if(isAnimating){
-        return
-    }
-    isAnimating = true;
+document.addEventListener('DOMContentLoaded', function () {
+  var navLinks = document.querySelectorAll('.navbar-collapse a');
+  var navBar = document.querySelector('.navbar-collapse');
+  var inscripcionButton = document.querySelector('#navLink-Inscripciones')
 
-    const imagen = document.querySelector('#inscripcion');
+  navLinks.forEach(function (link) {
+    link.addEventListener('click', function () {
+      if(link === inscripcionButton){
+        animateButton();
+      }
 
-    // Agregar la clase que muestra el borde de color
-    // imagen.classList.add('focus');
-
-    // Agregar la animación
-    imagen.classList.add('focus-anim');
-
-    // Remover la clase después de la animación
-    setTimeout(() => {
-        // imagen.classList.remove('focus');
-        imagen.classList.remove('focus-anim');
-        isAnimating = false;
-    }, 2000); // Duración de la animación en milisegundos
+      if (navBar.classList.contains('show')) {
+        setTimeout(() => {
+          new bootstrap.Collapse(navBar).toggle();
+        }, 700);
+      }
+    });
+  });
 });
+
+const animateButton = () => {
+  if (isAnimating) {
+    return
+  }
+  isAnimating = true;
+
+  const imagen = document.querySelector('#inscripcionButton');
+
+  // Agregar la clase que muestra el borde de color
+  // imagen.classList.add('focus');
+
+  // Agregar la animación
+  imagen.classList.add('focus-anim');
+
+  // Remover la clase después de la animación
+  setTimeout(() => {
+    // imagen.classList.remove('focus');
+    imagen.classList.remove('focus-anim');
+    isAnimating = false;
+  }, 2000); // Duración de la animación en milisegundos
+};
