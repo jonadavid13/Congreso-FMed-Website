8258988dd6415ed2aa94d12baab3f1cbbd0662cd
Jonathan Núñez <jonadavid13@gmail.com>
2026-05-02 13:36:24 -0400

    feat: enlace de redirección a Programa Científico (react-router)

---
+++ src/pages/ProgramaRedirect.jsx
@@ -0,0 +1,13 @@
+import { useEffect } from "react"
+
+const ProgramaRedirect = () => {
+	useEffect(() => {
+		const pdfProgramaPath = '/resources/PROGRAMA CIENTIFICO 2026.pdf'
+
+		window.location.replace(pdfProgramaPath)
+	}, [])
+
+  return null
+}
+
+export { ProgramaRedirect }
--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,19 +1,31 @@
 import { createBrowserRouter, Navigate } from "react-router";
 import { LandingPage } from "../pages/LandingPage";
 import { Enlaces } from "../pages/Enlaces";
+import { ProgramaRedirect } from "../pages/ProgramaRedirect";
 
-export const appRouter = createBrowserRouter([
+export const routes = [
 	{
 		path: "/",
 		element: <LandingPage />,
-
+		active: true
 	},
 	{
 		path: "/enlaces",
-		element: <Enlaces />
+		element: <Enlaces />,
+		active: false
+	},
+	{
+		path: "/programa-cientifico",
+		element: <ProgramaRedirect />,
+		active: true
 	},
 	{
 		path: "*",
-		element: <Navigate to="/" replace />
+		element: <Navigate to="/" replace />,
+		active: true
 	}
-]);
+]
+
+const activeRoutes = routes.filter((route) => route.active)
+
+export const appRouter = createBrowserRouter(activeRoutes)
